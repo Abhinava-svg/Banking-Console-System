35cfbce76c6c987bafbfdb0e7f473d51e035b6d1
Abhinava-svg <shrivastavaabhinava52@gmail.com>
2026-09-08 13:43:17 +0530

    Building Main.java file

--- main/src/main/java/com/banking/Main.java
+++ main/src/main/java/com/banking/Main.java
@@ -1,5 +1,19 @@
 package com.banking;
 
+import com.banking.Model.Account;
+import com.banking.Model.Customer;
+import com.banking.service.Bank;
 public class Main {
-    
+    public static void main(String[] args) throws Exception {
+        Bank bank = new Bank();
+
+        Customer obj = new Customer(101, "Rahul", "rahul@gmail.com");
+        bank.createCustomer(obj);
+
+        Account acc = new Account(54875, obj, 100000);
+        bank.createAccount(acc);
+
+        System.out.println("Customer created:" + obj);
+        System.out.println("Account created:" + acc);
+    }
 }
